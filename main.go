831edfcc3ade93fa8773a639013469b5c423f06c
Florian Barth <florianbarth@gmx.de>
2017-05-11 10:13:43 +0200

    Add documentation to package and functions

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
+// Copyright (C) {2017}  {Florian Barth florianbarth@gmx.de}
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// The osm_label_server delivers an REST endpoint which clients that
+// display openstreetmap data can use to obtain labels which can
+// be displayed on top of the tiles from the tile-server. This enables
+// clients to rotate the map without rotating the labels. It should be
+// used as follows
 package main
 
 import (
@@ -19,6 +39,8 @@ import (
 */
 import "C"
 
+// Label is a go representation of the labels that are returned by the
+// label-library
 type Label struct {
 	X     float64 `json:"x"`
 	Y     float64 `json:"y"`
@@ -30,6 +52,9 @@ type Label struct {
 	Label  string  `json:"label"`
 }
 
+// ds contains the datastructure which the label-library creates and
+// uses. It can only be used through the functions C.get_data and
+// C.is_good.
 var ds *C.Datastructure
 
 func main() {
@@ -46,6 +71,11 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
+// getLabels is the handler for the endpoint "/label". It parses the
+// arguments "x_min", "x_max", "y_min", "y_max" and "t_min" as
+// FormValues from the request and passes them to the Label
+// library. The obtained result is then transformed into go data types
+// and sent to the client json encoded
 func getLabels(w http.ResponseWriter, r *http.Request) {
 	x_min, err := tryParsingFormValue(w, r, "x_min")
 	if err != nil {
@@ -74,6 +104,8 @@ func getLabels(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(labels)
 }
 
+// cLabelToLabel takes a pointer to a C_Label given by the
+// label-library and turns it into the go-struct Label
 func cLabelToLabel(clabel *C.C_Label) Label {
 	label := Label{
 		float64(clabel.x),
@@ -87,6 +119,8 @@ func cLabelToLabel(clabel *C.C_Label) Label {
 	return label
 }
 
+// resultToLabelt iterates over the C-style array stored in C_Result
+// and converts it into a slice of Label
 func resultToLabels(result C.C_Result) []Label {
 	labels := make([]Label, result.size)
 	start := uintptr(unsafe.Pointer(result.data))
@@ -99,9 +133,9 @@ func resultToLabels(result C.C_Result) []Label {
 	return labels
 }
 
-// parseStringToCDouble takes a string and convers it
-// first into a float (returning an error if this is not possible)
-// and then into a C double (C.double)
+// parseStringToCDouble takes a string and convers it first into a
+// float (returning an error if this is not possible) and then into a
+// C double (C.double)
 func parseStringToCDouble(s string) (C.double, error) {
 	f, err := strconv.ParseFloat(s, 64)
 	if err != nil {
@@ -111,9 +145,9 @@ func parseStringToCDouble(s string) (C.double, error) {
 	return d, nil
 }
 
-// tryParsingFormValue gets the form value fromKey from r and tries
-// to convert it to double. If this fails, an error message will be written
-// to w and an error will be returned to the caller
+// tryParsingFormValue gets the form value fromKey from r and tries to
+// convert it to double. If this fails, an error message will be
+// written to w and an error will be returned to the caller
 func tryParsingFormValue(w http.ResponseWriter, r *http.Request, formKey string) (C.double, error) {
 
 	d, err := parseStringToCDouble((r.FormValue(formKey)))
