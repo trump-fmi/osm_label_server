13df939b00230bcb37f5c820ff4c83ef8779bc0a
Matthias Wagner <matzew.mail@gmail.com>
2017-05-31 15:11:15 +0200

    Licence header updated

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (C) {2017}  {Florian Barth florianbarth@gmx.de}
+// Copyright (C) {2017}  {Florian Barth florianbarth@gmx.de, Matthias Wagner matzew.mail@gmail.com}
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
